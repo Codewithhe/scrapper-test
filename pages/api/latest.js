74b8c43d363b147faa276c54017a2f9a1604a32b
shivamsharma <shivam.w2c@gmail.com>
2026-05-14 19:56:14 +0530

    tets

--- pages/api/latest.js
+++ pages/api/latest.js
@@ -1,21 +1,79 @@
-import dbConnect from "../../lib/mongodb";
-import Result from "../../models/Result";
+import axios from "axios";
 
 export default async function handler(req, res) {
 
    try {
 
-      await dbConnect();
+      /*
+      -------------------------
+      GET HOME PAGE
+      -------------------------
+      */
 
-      const latest = await Result.findOne()
-      .sort({ createdAt: -1 });
+      const home = await axios.get(
+         "https://sattagloble.com",
+         {
+            headers: {
+               "User-Agent":
+                  "Mozilla/5.0"
+            }
+         }
+      );
+
+      /*
+      -------------------------
+      GET COOKIES
+      -------------------------
+      */
+
+      const cookies = home.headers["set-cookie"]
+         ?.map(cookie => cookie.split(";")[0])
+         .join("; ");
+
+      /*
+      -------------------------
+      FETCH AJAX API
+      -------------------------
+      */
+
+      const response = await axios.post(
+         "https://sattagloble.com/ajax_index",
+         {},
+         {
+            headers: {
+
+               "Content-Type":
+                  "application/json",
+
+               "X-Requested-With":
+                  "XMLHttpRequest",
+
+               "Referer":
+                  "https://sattagloble.com/",
+
+               "User-Agent":
+                  "Mozilla/5.0",
+
+               "Cookie":
+                  cookies || ""
+            }
+         }
+      );
+
+      /*
+      -------------------------
+      RETURN DATA
+      -------------------------
+      */
 
       return res.status(200).json({
          success: true,
-         data: latest
+         data: response.data
       });
 
    } catch (err) {
+
+      console.log(err.message);
 
       return res.status(500).json({
          success: false,
